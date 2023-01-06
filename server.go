c878c6e026f79633d25f9bcdfd2f652e7aa9f48c
Naowal Siripatana <naowalzaza@gmail.com>
2023-01-06 09:12:44 +0700

    Could connect postgres db

--- server.go
+++ server.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	//db, err := sql.Open("postgres", "irtiqnti:I1wJ5UtoD9us3QWT5kHic5DCvwdvSyIj@rosie.db.elephantsql.com/irtiqnt")
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer db.Close()
+
+	log.Println("okay")
 }
